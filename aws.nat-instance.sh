d07c2af22c230c96fedf428941e4b11721620edb
yakosei <yakosei@users.noreply.github.com>
2017-01-21 01:44:46 +0900

    Create aws.nat-instance.sh

---
+++ aws.nat-instance.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+iptables -t nat -A POSTROUTING -o eth0 -s 0.0.0.0/0 -j MASQUERADE
+service iptables save
+sysctl -w net.ipv4.ip_forward=1
+sysctl -w net.ipv4.conf.eth0.send_redirects=0
+mkdir -p /etc/sysctl.d
+cat <<__EOD__ >/etc/sysctl.d/nat.conf
+net.ipv4.ip_forward = 1
+net.ipv4.conf.eth0.send_redirects = 0
+__EOD__
