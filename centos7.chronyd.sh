49342a677fe66419a7387d1dcff620c3d387b07c
yakosei <yakosei@users.noreply.github.com>
2017-01-22 01:07:54 +0900

    Create centos7.chronyd.sh

---
+++ centos7.chronyd.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+prefix_org=$(date +'-%Y%m%d-%H%M%S.org')
+yum -y install chrony
+ntp_srv=ntp.nict.jp
+ntp_conf=/etc/chrony.conf
+\cp -p $ntp_conf $ntp_conf$prefix_org
+sed -r 's/^server 0.[cr][a-z]+.pool.ntp.org/# '"$edit_comment"'\nserver '$ntp_srv'/g' $ntp_conf -i
+[ "$(diff $ntp_conf $ntp_conf$prefix)" ] || rm -f $ntp_conf$prefix
+systemctl start chronyd
+systemctl enable chronyd
+chronyc -a makestep
