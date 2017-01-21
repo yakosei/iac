f882e3b813886d505bb5c4eee1bb663fb19a336d
yakosei <yakosei@users.noreply.github.com>
2017-01-22 01:02:46 +0900

    Update centos7.selinux.disabled.sh

--- centos7.selinux.disabled.sh
+++ centos7.selinux.disabled.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-prefix=$(date +'-%Y%m%d-%H%M%S.org')
+prefix_org=$(date +'-%Y%m%d-%H%M%S.org')
 selinux_conf=/etc/selinux/config
 setenforce 0
-cp -p $selinux_conf $selinux_conf$prefix
+cp -p $selinux_conf $selinux_conf$prefix_org
 sed -r 's/^[ \t]*(SELINUX=[ep].+)/#\1\n# '"$(date +'Edit %Y%m%d by iac')"'\nSELINUX=disabled/g' $selinux_conf -i
-[ "$(diff $selinux_conf $selinux_conf$prefix)" ] || rm -f $selinux_conf$prefix
+[ "$(diff $selinux_conf $selinux_conf$prefix_org)" ] || rm -f $selinux_conf$prefix
