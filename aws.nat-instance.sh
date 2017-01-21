fcc236cb5fc0892346cca78fffd9cb92755e764b
yakosei <yakosei@users.noreply.github.com>
2017-01-22 00:42:34 +0900

    Update aws.nat-instance.sh

--- aws.nat-instance.sh
+++ aws.nat-instance.sh
@@ -5,7 +5,7 @@ sysctl -w net.ipv4.ip_forward=1
 sysctl -w net.ipv4.conf.eth0.send_redirects=0
 mkdir -p /etc/sysctl.d
 cat <<__EOD__ >/etc/sysctl.d/nat.conf
-# Edit $(date +%Y%m%s) by iac
+# Edit $(date +%Y%m%d) by iac
 net.ipv4.ip_forward = 1
 net.ipv4.conf.eth0.send_redirects = 0
 __EOD__
