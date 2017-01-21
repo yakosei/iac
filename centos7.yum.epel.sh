fdbe8b278ba9f41f9b688ca37f0e16a45d945396
yakosei <yakosei@users.noreply.github.com>
2017-01-22 00:39:44 +0900

    Create centos7.yum.epel.sh

---
+++ centos7.yum.epel.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+epel_dir='http://dl.fedoraproject.org/pub/epel/7/x86_64/e/'
+epel_url=$epel_dir$(curl -kLRs $epel_dir | sed -r -e '/epel-release/!d' -e 's/^.*href="(epel-release.*\.rpm)".*$/\1/g')
+yum -y install $epel_url
+yum-config-manager --disable epel*
