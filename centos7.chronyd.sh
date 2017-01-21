0404a8aa5c6d2ab3d16948fa410c4c48eaf5d510
yakosei <yakosei@users.noreply.github.com>
2017-01-22 01:15:40 +0900

    Update centos7.chronyd.sh

--- centos7.chronyd.sh
+++ centos7.chronyd.sh
@@ -4,7 +4,7 @@ yum -y install chrony
 ntp_srv=ntp.nict.jp
 ntp_conf=/etc/chrony.conf
 \cp -p $ntp_conf $ntp_conf$prefix_org
-sed -r 's/^(server 0.[cr][a-z]+.pool.ntp.org).*/# '"$(date +'Edit %Y%m%d by iac')"'\n#\1\nserver '$ntp_srv'/g' $ntp_conf -i
+sed -r 's/^(server 0.[cr][a-z]+.pool.ntp.org iburst).*/# '"$(date +'Edit %Y%m%d by iac')"'\n#\1\nserver '$ntp_srv' iburst/g' $ntp_conf -i
 [ "$(diff $ntp_conf $ntp_conf$prefix_org)" ] || rm -f $ntp_conf${prefix_org:?}
 systemctl start chronyd
 systemctl enable chronyd
