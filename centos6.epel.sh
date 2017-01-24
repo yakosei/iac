18ac45ebb359d31f4f0ae635504f74fbf6922682
夜行性SE <yakosei@users.noreply.github.com>
2017-01-24 22:08:16 +0900

    Create centos6.epel.sh

---
+++ centos6.epel.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+var_epel_dir='http://dl.fedoraproject.org/pub/epel/6/x86_64/'
+var_epel_url=$var_epel_dir$(curl -kLRs $var_epel_dir | sed -r -e '/epel-release/!d' -e 's/^.*href="(epel-release.*\.rpm)".*$/\1/g')
+yum -y install yum-utils $var_epel_url
+yum-config-manager --disable epel*
