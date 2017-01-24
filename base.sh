c8e148ead63ede06c26e9c89578da8c594f20d6a
夜行性SE <yakosei@users.noreply.github.com>
2017-01-24 21:34:40 +0900

    Update and rename base.sh to export_var.sh

--- base.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-prefix_org=$(date +'-%Y%m%d-%H%M%S.org')
-comment=$(date +'Edit %Y%m%d by iac')
---
+++ export_var.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+var_prefix_org=$(date +'-%Y%m%d-%H%M%S.org')
+var_comment=$(date +'Edit %Y%m%d by iac')
