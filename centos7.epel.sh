eb6640ac6c2fc5bb5ad8e19dfbc408afd2c08e2a
夜行性SE <yakosei@users.noreply.github.com>
2017-01-24 22:07:42 +0900

    Update centos7.epel.sh

--- centos7.epel.sh
+++ centos7.epel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-epel_dir='http://dl.fedoraproject.org/pub/epel/7/x86_64/e/'
-epel_url=$epel_dir$(curl -kLRs $epel_dir | sed -r -e '/epel-release/!d' -e 's/^.*href="(epel-release.*\.rpm)".*$/\1/g')
-yum -y install yum-utils $epel_url
+var_epel_dir='http://dl.fedoraproject.org/pub/epel/7/x86_64/e/'
+var_epel_url=$var_epel_dir$(curl -kLRs $var_epel_dir | sed -r -e '/epel-release/!d' -e 's/^.*href="(epel-release.*\.rpm)".*$/\1/g')
+yum -y install yum-utils $var_epel_url
 yum-config-manager --disable epel*
