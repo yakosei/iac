2a1135719855c79483e40c926690443bb1607e0c
夜行性SE <yakosei@users.noreply.github.com>
2017-01-24 22:06:10 +0900

    Update centos7.chrony.sh

--- centos7.chrony.sh
+++ centos7.chrony.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-prefix_org=$(date +'-%Y%m%d-%H%M%S.org')
+var_prefix_org=$(date +'-%Y%m%d-%H%M%S.org')
 yum -y install chrony
-ntp_srv=ntp.nict.jp
-ntp_conf=/etc/chrony.conf
-\cp -p $ntp_conf $ntp_conf${prefix_org:?}
-sed -r 's/^[ \t]*(server 0.[a-z]+.pool.ntp.org iburst.*)/# '"$(date +'Edit %Y%m%d by iac')"'\n#\1\nserver '$ntp_srv' iburst/g' $ntp_conf -i
-[ "$(diff $ntp_conf $ntp_conf$prefix_org)" ] || rm -f $ntp_conf$prefix_org
+var_ntp_srv=ntp.nict.jp
+var_ntp_conf=/etc/chrony.conf
+\cp -p $var_ntp_conf $var_ntp_conf${var_prefix_org:?}
+sed -r -e 's/^[ \t]*(server 0.[a-z]+.pool.ntp.org iburst.*)/# '"$(date +'Edit %Y%m%d by iac')"'\n#\1\nserver '$var_ntp_srv' iburst/g' $var_ntp_conf -i
+[ "$(diff $var_ntp_conf $var_ntp_conf$var_prefix_org)" ] || \mv -f $var_ntp_conf$var_prefix_org $var_ntp_conf
 systemctl start chronyd
 systemctl enable chronyd
 chronyc -a makestep
