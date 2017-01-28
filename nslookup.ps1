81d67b255048522ddf8a5b10bc22bf07dd45314b
夜行性SE <yakosei@users.noreply.github.com>
2017-01-28 14:33:58 +0900

    Update nslookup.ps1

--- nslookup.ps1
+++ nslookup.ps1
@@ -1,6 +1,6 @@
 while (1)
 {
-    Get-Date -Format "==== yyyy-MM-ddTHH:mm:ss ============================"
+    Get-Date -Format "==== yyyy-MM-ddTHH:mm:ss ================================"
     nslookup dav.progdence.co.jp 2>$null
     Write-Output ""
     Start-Sleep -s 1
