ced0598f9a0ea8fc22c0a533168012a0afa46afe
夜行性SE <yakosei@users.noreply.github.com>
2017-02-05 08:46:32 +0900

    Update centos7.selinux.sh

--- centos7.selinux.sh
+++ centos7.selinux.sh
@@ -5,3 +5,4 @@ setenforce 0
 \cp -p $var_selinux_conf $var_selinux_conf${var_prefix_org:?}
 sed -r -e 's/^[ \t]*(SELINUX=[ep].+)/#\1\n# '"$(date +'Edit %Y%m%d by iac')"'\nSELINUX=disabled/g' $var_selinux_conf -i
 [ "$(diff $var_selinux_conf $var_selinux_conf$var_prefix_org)" ] || \mv -f $var_selinux_conf$var_prefix_org $var_selinux_conf
+getenforce
