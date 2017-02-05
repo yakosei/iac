909494159168226f91f64a316f778dfb43fcc056
夜行性SE <yakosei@users.noreply.github.com>
2017-02-05 09:07:54 +0900

    Update centos6.locale.sh

--- centos6.locale.sh
+++ centos6.locale.sh
@@ -7,7 +7,7 @@ cat $var_locale_conf
 echo
 
 \cp -p $var_locale_conf $var_locale_conf${var_prefix_org:?}
-[ "$(grep 'LANG="en_US.UTF-8"' $var_locale_conf)" ] || sed -r -e 's/^(LANG=.*)$/# '"$(date +'Edit %Y%m%d by iac')"'#\1\nLANG="en_US.UTF-8"/g' $var_locale_conf -i
+[ "$(grep 'LANG="en_US.UTF-8"' $var_locale_conf)" ] || sed -r -e 's/^(LANG=.*)$/# '"$(date +'Edit %Y%m%d by iac')"'\n#\1\nLANG="en_US.UTF-8"/g' $var_locale_conf -i
 [ "$(diff $var_locale_conf $var_locale_conf$var_prefix_org)" ] || \mv -f $var_locale_conf$var_prefix_org $var_locale_conf
 
 . $var_locale_conf
