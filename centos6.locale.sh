4d3fe773988dd94258d6a62d71c1b7a953cd79ca
夜行性SE <yakosei@users.noreply.github.com>
2017-02-05 09:06:52 +0900

    Create centos6.locale.sh

---
+++ centos6.locale.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+var_prefix_org=$(date +'-%Y%m%d-%H%M%S.org')
+var_locale_conf=/etc/sysconfig/i18n
+
+echo "==> $var_locale_conf <=="
+cat $var_locale_conf
+echo
+
+\cp -p $var_locale_conf $var_locale_conf${var_prefix_org:?}
+[ "$(grep 'LANG="en_US.UTF-8"' $var_locale_conf)" ] || sed -r -e 's/^(LANG=.*)$/# '"$(date +'Edit %Y%m%d by iac')"'#\1\nLANG="en_US.UTF-8"/g' $var_locale_conf -i
+[ "$(diff $var_locale_conf $var_locale_conf$var_prefix_org)" ] || \mv -f $var_locale_conf$var_prefix_org $var_locale_conf
+
+. $var_locale_conf
