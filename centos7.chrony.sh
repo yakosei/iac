49245571bc89a7aa895e0192733c59023b00f3ba
夜行性SE <yakosei@users.noreply.github.com>
2017-02-05 15:31:11 +0900

    Update centos7.chrony.sh

--- centos7.chrony.sh
+++ centos7.chrony.sh
@@ -3,11 +3,11 @@ var_prefix_org=$(date +'-%Y%m%d-%H%M%S.org')
 
 yum -y install chrony
 
+var_conf=/etc/chrony.conf
 var_ntp_srv=ntp.nict.jp
-var_ntp_conf=/etc/chrony.conf
-\cp -p $var_ntp_conf $var_ntp_conf${var_prefix_org:?}
-sed -r -e 's/^[ \t]*(server 0.[a-z]+.pool.ntp.org iburst.*)/# '"$(date +'Edit %Y%m%d by iac')"'\n#\1\nserver '$var_ntp_srv' iburst/g' $var_ntp_conf -i
-[ "$(diff $var_ntp_conf $var_ntp_conf$var_prefix_org)" ] || \mv -f $var_ntp_conf$var_prefix_org $var_ntp_conf
+\cp -p $var_conf $var_conf${var_prefix_org:?}
+sed -r -e 's/^[ \t]*(server 0.[a-z]+.pool.ntp.org iburst.*)/# '"$(date +'Edit %Y%m%d by iac')"'\n#\1\nserver '$var_ntp_srv' iburst/g' $var_conf -i
+[ "$(diff $var_conf $var_conf$var_prefix_org)" ] || \mv -f $var_conf$var_prefix_org $var_conf
 
 systemctl start chronyd
 systemctl enable chronyd
