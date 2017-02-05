761769f2b9ba32fe324f742c7f2631f2a2660343
夜行性SE <yakosei@users.noreply.github.com>
2017-02-05 15:32:24 +0900

    Update centos6.locale.sh

--- centos6.locale.sh
+++ centos6.locale.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 var_prefix_org=$(date +'-%Y%m%d-%H%M%S.org')
-var_locale_conf=/etc/sysconfig/i18n
+var_conf=/etc/sysconfig/i18n
 
-\cp -p $var_locale_conf $var_locale_conf${var_prefix_org:?}
-[ "$(grep 'LANG="en_US.UTF-8"' $var_locale_conf)" ] || sed -r -e 's/^(LANG=.*)$/# '"$(date +'Edit %Y%m%d by iac')"'\n#\1\nLANG="en_US.UTF-8"/g' $var_locale_conf -i
-[ "$(diff $var_locale_conf $var_locale_conf$var_prefix_org)" ] || \mv -f $var_locale_conf$var_prefix_org $var_locale_conf
+\cp -p $var_conf $var_conf${var_prefix_org:?}
+[ "$(grep 'LANG="en_US.UTF-8"' $var_conf)" ] || sed -r -e 's/^(LANG=.*)$/# '"$(date +'Edit %Y%m%d by iac')"'\n#\1\nLANG="en_US.UTF-8"/g' $var_conf -i
+[ "$(diff $var_conf $var_conf$var_prefix_org)" ] || \mv -f $var_conf$var_prefix_org $var_conf
 
-. $var_locale_conf
+. $var_conf
+
