4420c74e7ef548727e74dde9f5506f0bb4e618d2
夜行性SE <yakosei@users.noreply.github.com>
2017-02-05 09:52:01 +0900

    Update aws.nat-instance.sh

--- aws.nat-instance.sh
+++ aws.nat-instance.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+
 iptables -t nat -A POSTROUTING -o eth0 -s 0.0.0.0/0 -j MASQUERADE
 service iptables save
+
 sysctl -w net.ipv4.ip_forward=1
 sysctl -w net.ipv4.conf.eth0.send_redirects=0
+
 mkdir -p /etc/sysctl.d
 cat <<__EOD__ >/etc/sysctl.d/nat.conf
 # Edit $(date +%Y%m%d) by iac
