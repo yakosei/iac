e432aebe5f44d078ad0d580538e8872f45eecb12
夜行性SE <yakosei@users.noreply.github.com>
2017-02-05 09:12:03 +0900

    Update centos7.general.sh

--- centos7.general.sh
+++ centos7.general.sh
@@ -3,6 +3,6 @@ curl iac.yakosei.com/centos7.utils.sh | bash
 curl iac.yakosei.com/centos7.selinux.sh | bash
 curl iac.yakosei.com/centos7.locale.sh | bash
 curl iac.yakosei.com/centos7.chrony.sh | bash
-mkdir -p -m 700 ~/.ssh; curl iac.yakosei.com/keys >>authorized_keys
+[ -f ~/.ssh/authorized_keys ] || mkdir -p -m 700 ~/.ssh; curl iac.yakosei.com/keys >>~/.ssh/authorized_keys
 curl iac.yakosei.com/profile.sh | bash
 curl iac.yakosei.com/vimrc.sh | bash
