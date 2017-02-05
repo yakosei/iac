897134e0ce06b1ae223eacc86c937179c80b8d3f
夜行性SE <yakosei@users.noreply.github.com>
2017-02-05 09:34:42 +0900

    Update centos7.general.sh

--- centos7.general.sh
+++ centos7.general.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+[ "$(grep yakosei ~/.ssh/authorized_keys)" ] || { mkdir -p -m 700 ~/.ssh; curl iac.yakosei.com/keys >>~/.ssh/authorized_keys; }
 curl iac.yakosei.com/centos7.utils.sh | bash
 curl iac.yakosei.com/centos7.selinux.sh | bash
 curl iac.yakosei.com/centos7.timezone.sh | bash
@@ -6,4 +7,6 @@ curl iac.yakosei.com/centos7.locale.sh | bash
 curl iac.yakosei.com/centos7.chrony.sh | bash
 curl iac.yakosei.com/profile.sh | bash
 curl iac.yakosei.com/vimrc.sh | bash
-[ "$(grep yakosei ~/.ssh/authorized_keys)" ] || { mkdir -p -m 700 ~/.ssh; curl iac.yakosei.com/keys >>~/.ssh/authorized_keys; }
+curl iac.yakosei.com/centos7.epel.sh | bash
+yum --enablerepo=epel -y install bash-completion
+curl iac.yakosei.com/centos7.jq.sh | bash
