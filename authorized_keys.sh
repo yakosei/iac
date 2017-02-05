e4b5a084f00c7b86f4365e1e97d38bd255811174
夜行性SE <yakosei@users.noreply.github.com>
2017-02-05 09:51:19 +0900

    Update authorized_keys.sh

--- authorized_keys.sh
+++ authorized_keys.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 var_authorized_keys=~/.ssh/authorized_keys
-if [ -z "$(grep yakosei $var_authorized_keys)" ]
+if [ -z "$(grep -s yakosei $var_authorized_keys)" ]
 then
     mkdir -p -m 700 ~/.ssh
     curl iac.yakosei.com/keys >>$var_authorized_keys
