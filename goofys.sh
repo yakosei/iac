7758eed023f15b0bf81eed0386c6cba6d5b803b9
夜行性SE <yakosei@users.noreply.github.com>
2017-02-08 09:54:35 +0900

    Update goofys.sh

--- goofys.sh
+++ goofys.sh
@@ -4,12 +4,17 @@ chmod 755 /tmp/goofys
 \mv /tmp/goofys /usr/bin/goofys
 
 cat <<'__EOD__'
+var_prefix_org=$(date +'-%Y%m%d-%H%M%S.org')
+var_conf=/etc/fstab
+var_bin=/usr/bin/goofys
+S3MOUNTDIR=''
+S3BUCKET=''
+
 [ -d $S3MOUNTDIR ] || mkdir -p $S3MOUNTDIR
 goofys --region $(curl -s --connect-timeout 4 http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/.$//') $S3BUCKET $S3MOUNTDIR
 sleep 20
 [ "$(df | grep $S3BUCKET)" ] || { echo 'mount $S3BUCKET failed.' && exit 1; }
-fstab=/etc/fstab
-cp -p $fstab $fstab$prefix
-[ "$(grep "$S3BUCKET" $fstab)" ] || echo -e "\n# $edit_comment\n$goofys_bin#$S3BUCKET\t$S3MOUNTDIR\tfuse\t_netdev,allow_other,--file-mode=0666\t0\t0" >>$fstab
-[ "$(diff $fstab $fstab$prefix)" ] || rm -f $fstab$prefix
+\cp -p $var_conf $var_conf$var_prefix_org
+[ "$(grep "$S3BUCKET" $var_conf)" ] || echo -e "\n# $(date +'Edit %Y%m%d')\n$var_bin#$S3BUCKET\t$S3MOUNTDIR\tfuse\t_netdev,allow_other,--file-mode=0666\t0\t0" >>$var_conf
+[ "$(diff $var_conf $var_conf$var_prefix_org)" ] || rm -f $var_conf$var_prefix_org
 __EOD__
