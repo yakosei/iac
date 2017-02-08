0ea07eb91c4f5e39344b830781f0a85f5b666c90
夜行性SE <yakosei@users.noreply.github.com>
2017-02-08 09:50:56 +0900

    Update goofys.sh

--- goofys.sh
+++ goofys.sh
@@ -5,9 +5,9 @@ chmod 755 /tmp/goofys
 
 cat <<'__EOD__'
 [ -d $S3MOUNTDIR ] || mkdir -p $S3MOUNTDIR
-goofys --region $aws_region $S3BUCKET $S3MOUNTDIR
+goofys --region $(curl -s --connect-timeout 4 http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/.$//') $S3BUCKET $S3MOUNTDIR
 sleep 20
-[ "$(df | grep $S3BUCKET)" ] || halt
+[ "$(df | grep $S3BUCKET)" ] || { echo 'mount $S3BUCKET failed.' && exit 1; }
 fstab=/etc/fstab
 cp -p $fstab $fstab$prefix
 [ "$(grep "$S3BUCKET" $fstab)" ] || echo -e "\n# $edit_comment\n$goofys_bin#$S3BUCKET\t$S3MOUNTDIR\tfuse\t_netdev,allow_other,--file-mode=0666\t0\t0" >>$fstab
