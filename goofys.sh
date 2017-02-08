f8e922f18d8cb2ed51c1f5f28c1e89ba9e2462d5
夜行性SE <yakosei@users.noreply.github.com>
2017-02-08 10:14:00 +0900

    Update goofys.sh

--- goofys.sh
+++ goofys.sh
@@ -13,7 +13,7 @@ S3BUCKET=''
 [ -d $S3MOUNTDIR ] || mkdir -p $S3MOUNTDIR
 goofys --region $(curl -s --connect-timeout 4 http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/.$//') $S3BUCKET $S3MOUNTDIR
 sleep 20
-[ "$(df | grep $S3BUCKET)" ] || { echo 'mount $S3BUCKET failed.' && exit 1; }
+[ "$(df -P | grep $S3BUCKET)" ] || { echo 'mount $S3BUCKET failed.' && exit 1; }
 \cp -p $var_conf $var_conf$var_prefix_org
 [ "$(grep "$S3BUCKET" $var_conf)" ] || echo -e "\n# $(date +'Edit %Y%m%d')\n$var_bin#$S3BUCKET\t$S3MOUNTDIR\tfuse\t_netdev,allow_other,--file-mode=0666\t0\t0" >>$var_conf
 [ "$(diff $var_conf $var_conf$var_prefix_org)" ] || rm -f $var_conf$var_prefix_org
