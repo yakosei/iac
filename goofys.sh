b13256d053c7da302da93364e4cc423d092b5728
夜行性SE <yakosei@users.noreply.github.com>
2017-02-08 09:42:36 +0900

    Update goofys.sh

--- goofys.sh
+++ goofys.sh
@@ -1,4 +1,4 @@
 yum -y install fuse
 curl -kLR https://github.com/kahing/goofys/releases/download/v0.0.9/goofys -o /tmp/goofys
-chmod 755 goofys
+chmod 755 /tmp/goofys
 \mv /tmp/goofys /usr/bin/goofys
