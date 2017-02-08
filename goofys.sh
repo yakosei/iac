389ebab977c5ebb7bcdf2d524eee47b46b0b0267
夜行性SE <yakosei@users.noreply.github.com>
2017-02-08 09:47:53 +0900

    Update goofys.sh

--- goofys.sh
+++ goofys.sh
@@ -2,3 +2,14 @@ yum -y install fuse
 curl -kLR https://github.com/kahing/goofys/releases/download/v0.0.9/goofys -o /tmp/goofys
 chmod 755 /tmp/goofys
 \mv /tmp/goofys /usr/bin/goofys
+
+cat <<'__EOD__'
+[ -d $S3MOUNTDIR ] || mkdir -p $S3MOUNTDIR
+goofys --region $aws_region $S3BUCKET $S3MOUNTDIR
+sleep 20
+[ "$(df | grep $S3BUCKET)" ] || halt
+fstab=/etc/fstab
+cp -p $fstab $fstab$prefix
+[ "$(grep "$S3BUCKET" $fstab)" ] || echo -e "\n# $edit_comment\n$goofys_bin#$S3BUCKET\t$S3MOUNTDIR\tfuse\t_netdev,allow_other,--file-mode=0666\t0\t0" >>$fstab
+[ "$(diff $fstab $fstab$prefix)" ] || rm -f $fstab$prefix
+__EOD__
