41a7bf341a6b67870b8fd733c5f3fc9f94aad41e
夜行性SE <yakosei@users.noreply.github.com>
2017-02-08 16:27:35 +0900

    Update nslookup.ps1

--- nslookup.ps1
+++ nslookup.ps1
@@ -1,7 +1,1 @@
-while (1)
-{
-    Get-Date -Format "==== yyyy-MM-ddTHH:mm:ss ================================"
-    nslookup iac.yakosei.com 2>$null
-    Write-Output ""
-    Start-Sleep -s 1
-}
+while (1) { Get-Date -Format "==== yyyy-MM-ddTHH:mm:ss ================================"; nslookup iac.yakosei.com 2>$null; Write-Output ""; Start-Sleep -s 1; }
