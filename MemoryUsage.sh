a336ea6526f903ac53f848f15f1fc0bc54920550
夜行性SE <yakosei@users.noreply.github.com>
2017-02-14 18:49:07 +0900

    Update MemoryUsage.sh

--- MemoryUsage.sh
+++ MemoryUsage.sh
@@ -1,3 +1,2 @@
 # CentOS 6
-cat /proc/meminfo | awk 'BEGIN{i=0;j=0;k=0;l=0}; /^MemTotal:/{i=$2}; /^MemFree:/{j=$2}; /^Active\(file\):/{k=$2};
-/^Inactive\(file\):/{l=$2}; END{print (i-j-k-l)*100/i}'
+cat /proc/meminfo | awk 'BEGIN{i=0;j=0;k=0;l=0}; /^MemTotal:/{i=$2}; /^MemFree:/{j=$2}; /^Active\(file\):/{k=$2}; /^Inactive\(file\):/{l=$2}; END{print (i-j-k-l)*100/i}'
